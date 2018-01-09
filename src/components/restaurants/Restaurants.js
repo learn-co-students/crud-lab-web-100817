714143314b34d38e77d66cf7c6054e4129ccf799
github username <github email address>
2018-01-09 14:51:07 -0500

    Done.

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import Restaurant from './Restaurant'
 
 class Restaurants extends Component {
+
   render() {
+
+    console.log("restaurants", this.props);
+
+    const restaurants = this.props.store.getState().restaurants.map((restaurant, index) => {
+      return <Restaurant key={index} restaurant={restaurant} store={this.props.store} />
+    })
+
     return(
       <ul>
         Restaurants Component
+        {restaurants}
       </ul>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,28 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
+export default function manageRestaurant(state = {
+  restaurants: [],
+  reviews: []
+}, action) {
+  // debugger
+  switch (action.type) {
+    case 'ADD_RESTAURANT':
+    const restaurant = {text: action.restaurant.text, id: cuidFn()}
+    return { restaurants: state.restaurants.concat(restaurant) }
+  case 'DELETE_RESTAURANT':
+    const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id);
+    return Object.assign({}, state, { restaurants });
+    case 'ADD_REVIEW':
+      const review = Object.assign({}, action.review, { id: cuidFn() });
+      return Object.assign({}, state, {
+        reviews: state.reviews.concat(review),
+      });
 
-}
+    case 'DELETE_REVIEW':
+      const reviews = state.reviews.filter(review => review.id !== action.id);
+      return Object.assign({}, state, { reviews });
+    default:
+      return state;
+  }
+};
